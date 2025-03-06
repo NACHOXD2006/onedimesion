47f1ad57782deb8d3abc397199622eeb71a8ca84
19795400@clases.edu.sv <19795400@clases.edu.sv>
2025-03-06 15:03:29 -0600

    primer comit

---
+++ Onedimensioarray19795400/OneDmsString.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OneDmsString.aspx.cs" Inherits="Onedimensioarray19795400.OneDmsString" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label1" runat="server" Text="Create Cateogry Array"></asp:Label>
+             <br />
+             <asp:Button ID="Button1" runat="server" Text="Create Array" OnClick="Button1_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Onedimensioarray19795400/OneDmsString.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Onedimensioarray19795400
+{
+    public partial class OneDmsString : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String[] strCategoryArray = new String[5];
+            strCategoryArray[0] = "Footwears - men's";
+            strCategoryArray[1] = "Clothing - men's";
+            strCategoryArray[2] = "Racquest";
+            strCategoryArray[3] = "Footwears - women's";
+            strCategoryArray[4] = "Clothing - women's";
+            Session["strCategoryArray"] = strCategoryArray;
+            Response.Redirect("CategoryUseArraySessionVariables.aspx");
+        }
+    }
+}
